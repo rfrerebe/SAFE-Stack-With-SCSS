2bb330c2c3acb17dbd1639eed56c89dcccae2653
Rémy Frèrebeau <remy@frerebeau.org>
2018-08-17 21:46:04 +0200

    Works !!!!

--- src/Client/webpack.config.js
+++ src/Client/webpack.config.js
@@ -1,11 +1,18 @@
 var path = require("path");
 var webpack = require("webpack");
 var fableUtils = require("fable-utils");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 function resolve(filePath) {
     return path.join(__dirname, filePath)
 }
 
+var CONFIG = {
+    indexHtmlTemplate: './public/index.html',
+    fsharpEntry: './Client.fsproj',
+    cssEntry: './scss/main.scss',
+    outputDir: './output',
+}
 var babelOptions = fableUtils.resolveBabelOptions({
     presets: [
         ["env", {
@@ -18,6 +25,12 @@ var babelOptions = fableUtils.resolveBabelOptions({
     plugins: ["transform-runtime"]
 });
 
+var commonPlugins = [
+    new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: CONFIG.indexHtmlTemplate
+    })
+];
 
 var isProduction = process.argv.indexOf("-p") >= 0;
 var port = process.env.SUAVE_FABLE_PORT || "8085";
@@ -25,12 +38,22 @@ console.log("Bundling for " + (isProduction ? "production" : "development") + ".
 
 module.exports = {
     devtool: "source-map",
-    entry: resolve('./Client.fsproj'),
+    // entry: resolve('./Client.fsproj'),
+    entry: isProduction ? {
+        app: [CONFIG.fsharpEntry, CONFIG.cssEntry]
+    } : {
+            app: [ CONFIG.fsharpEntry],
+            style: [CONFIG.cssEntry]
+    },
+
+
     mode: isProduction ? "production" : "development",
     output: {
-        path: resolve('./public/js'),
+        //path: resolve('./public/js'),
+        path: path.join(__dirname, CONFIG.outputDir),
         publicPath: "/js",
-        filename: "bundle.js"
+        // filename: "bundle.js"
+        filename: isProduction ? '[name].[hash].js' : '[name].js'
     },
     resolve: {
         symlinks: false,
@@ -66,11 +89,25 @@ module.exports = {
                     loader: 'babel-loader',
                     options: babelOptions
                 },
+            },
+            {
+                test: /\.(sass|scss|css)$/,
+                use: [
+                    isProduction
+                        ? MiniCssExtractPlugin.loader
+                        : 'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?.*)?$/,
+                use: ["file-loader"]
             }
         ]
     },
-    plugins: isProduction ? [] : [
+    plugins: isProduction ? [] :
+    commonPlugins.concat([
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NamedModulesPlugin()
-    ]
+    ]),
 };
